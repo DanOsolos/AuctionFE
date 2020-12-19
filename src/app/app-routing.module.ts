fb2c11f1efc5dbc69425abde4ff8473920cbf784
Dan Osolos <dan.stefan75@yahoo.com>
2020-12-19 12:12:16 +0200

    typo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [{ path: '', redirectTo: '/', pathMatch: 'full' },
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }];
 
